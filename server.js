afaea5d9637993d96b0a1d48db23ea34b585ec07
Fırat KÜÇÜK <firatkucuk@gmail.com>
2022-11-18 21:26:40 +0100

    task 1 completed

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+// Create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World'); // Write a response to the client
+  res.end(); // End the response
+}).listen(8080); // Server listens on port 8080
